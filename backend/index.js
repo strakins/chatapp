cd4745a07b3ce364ada7dd8ad4795c5a1fe63f25
strakins <akinolablessing25@gmail.com>
2024-02-14 16:37:12 +0100

    backend actions initiated

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import authRoutes from  './routes/authRoutes.js'
+
 
 dotenv.config();
 
@@ -12,9 +14,14 @@ app.get("/", (req, res) => {
     res.send("Hello World test Route!!")
 })
 
-app.get('/api/auth/signup', (req, res) => {
-    res.send("SignUp route")
-})
+app.use("/api/auth", authRoutes)
+
+// app.get('/api/auth/signup', (req, res) => {
+//     res.send("SignUp route")
+// })
+// app.get('/api/auth/login', (req, res) => {
+//     res.send("Login route")
+// })
 
 app.listen(PORT, () => {
     console.log(`Server Runnig on Port ${PORT}`)
